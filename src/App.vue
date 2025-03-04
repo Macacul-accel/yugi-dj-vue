0c207857bba7a48d4d93615e3b940ea4aa1f5cce
Leo LIU <leo_liu@hotmail.fr>
2025-03-04 17:57:36 +0100

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <!-- Navbar yugi-django -->
+  </header>
+
+  <RouterView />
+</template>
