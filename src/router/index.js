121af829ce19e0a855b6087140770ce138d52c88
Leo LIU <leo_liu@hotmail.fr>
2025-03-15 22:25:53 +0100

    💄 New favicon + Window title

--- src/router/index.js
+++ src/router/index.js
@@ -10,46 +10,62 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { title: 'Yugi-django'},
     },
     {
       path: '/register',
       name: 'register',
       component: RegisterView,
-      meta: { alreadyLogin: true },
+      meta: { 
+        alreadyLogin: true,
+        title: 'Inscription | Yugi-django',
+      },
     },
     {
       path: '/login',
       name: 'login',
       component: () => import ('../views/LoginView.vue'),
-      meta: { alreadyLogin: true },
+      meta: { 
+        alreadyLogin: true,
+        title: 'Connexion | Yugi-django',
+      },
     },
     {
       path: '/features',
       name: 'features',
       component: () => import ('../views/FeaturesView.vue'),
+      meta: { title: 'Fonctionnalités | Yugi-django'},
     },
     {
       path: '/cards',
       name: 'cards',
       component: () => import ('../views/CardsView.vue'),
+      meta: { title: 'Liste des cartes | Yugi-django'},
     },
     {
       path: '/decks',
       name: 'decks',
       component: () => import ('../views/DecksView.vue'),
-      meta: { requireLogin: true },
+      meta: { 
+        requireLogin: true,
+        title: 'Mes decks | Yugi-django',
+      },
     },
     {
       path: '/decks/:deckName',
       name: 'My deck',
       component: () => import ('../views/BuildDeckView.vue'),
-      meta: { requireLogin: true },
+      meta: { 
+        requireLogin: true,
+        title: 'Mon deck | Yugi-django',
+      },
     },
   ],
 })
 
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore()
+  document.title = to.meta.title || 'Yugi-django'
 
   if (to.meta.alreadyLogin && authStore.isAuthenticated) {
     next('/')
